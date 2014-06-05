770064ffe4722b4c178bc726349e48ebd057b0b1
lesanc <lesanc00@yahoo.com>
2014-06-05 18:55:21 -0500

    Primer función Easeljs

--- js/App.js
+++ js/App.js
@@ -1,3 +1,32 @@
-(function() {
-    // body...
-})
+console.log('hola 1');
+(function(scope) {
+    function App() {
+        this.initialize();
+
+        console.log("inicia App");
+    }
+
+    var canvas = App.prototype;
+    var stage = App.prototype;
+
+    App.prototype.initialize = function() {
+        this.canvas = document.createElement("canvas");
+        this.canvas.width = 600;
+        this.canvas.height = 600;
+
+        var contenedor = document.getElementById("juego");
+        contenedor.appendChild(this.canvas);
+
+        this.stage = new createjs.Stage(this.canvas);
+
+        console.debug(this.canvas);
+    };
+    scope.App = App;
+}(window));
+
+window.onload = function() {
+    console.log('hola mundo');
+    this.app = new App;
+
+    console.log("Llamada inicio App");
+};
