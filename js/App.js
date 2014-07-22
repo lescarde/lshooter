402c856b1754aca74dfabc0977e14b70880960d3
lesanc <lesanc00@yahoo.com>
2014-07-22 15:25:28 -0500

    Carga fondo --- Sonidos

--- js/App.js
+++ js/App.js
@@ -1,21 +1,23 @@
 (function(scope) {
     function App() {
         this.initialize();
+    };
 
-        console.log("inicia App");
-    }
+    var canvas = App.prototype;
+    var stage = App.prototype;
+    var cargador = App.prototype;
+    var fondo = App.prototype;
+    var nave = App.prototype;
 
-    var canvas      = App.prototype;
-    var stage       = App.prototype;
-    var cargador    = App.prototype;
-    
-    var rutaNave     = "assets/nave.png";
-    var rutaMalo     = "assets/malo.png";
-    var rutaFondo    = "assets/fondo.jpg";
+    var rutaNave = "assets/nave.png";
+    var rutaMalo = "assets/malo.png";
+    var rutaFondo = "assets/fondo.jpg";
     var rutaDispNave = "assets/DisparoNave.png";
 
     App.prototype.initialize = function() {
+
         var self = this;
+
         this.canvas = document.createElement("canvas");
         this.canvas.width = 600;
         this.canvas.height = 600;
@@ -24,26 +26,49 @@
         contenedor.appendChild(this.canvas);
 
         this.stage = new createjs.Stage(this.canvas);
-        
-        this.cargador = new Cargador();
-        this.cargador.onComplete = function(){
-            self.assetsCargados();
-        }
-        this.cargador.loadImagenes([rutaNave, rutaMalo, rutaFondo, rutaFondo]);
 
-        console.debug(this.canvas);
+        this.cargador = new Cargador();
+        this.cargador.onComplete = function() {
+            self.assetsCargados();
+            console.log("assets cargados");
+        };
+        this.cargador.loadImagenes([rutaNave, rutaMalo, rutaFondo, rutaDispNave]);
     };
-    
-    App.prototype.assetsCargados = function(){
-        console.log('imágenes cargadas satisfactoriamente');
+
+    App.prototype.assetsCargados = function() {
+        console.log("imágenes cargadas 38");
         console.debug(this.cargador);
+
+        var bmp = this.cargador[rutaFondo];
+        this.fondo = new createjs.Bitmap(bmp);
+
+        this.fondo.alpha = 0.8;
+        this.stage.addChild(this.fondo);
+
+        console.log("fondo cargado");
+        console.debug(this.fondo);
+
+        var self = this;
+
+        createjs.Ticker.setFPS(30);
+
+        console.debug(createjs.Ticker);
+        createjs.Ticker.addEventListener("tick", function(e) {
+            self.Tick();
+
+            console.log("self cargado");
+            console.debug(self);
+        });
     }
-    
+
+    App.prototype.Tick = function() {
+        this.stage.update();
+        console.log("Tick update");
+    };
+
     scope.App = App;
 }(window));
 
 window.onload = function() {
-    this.app = new App;
-
-    console.log("Llamada inicio App");
-};
+    this.app = new App();
+}
--- js/Cargador.js
+++ js/Cargador.js
@@ -1,47 +1,50 @@
-(function(scope){
+(function (scope) {
     function Cargador(){
         this.initialize();
-        
-        console.log('Inicia Cargador');
-    }
-    
+ };
+
     var cargadas = Cargador.prototype;
     var totales = Cargador.prototype;
     var onComplete = Cargador.prototype;
     
+    console.log("variables creadas");
+  
     Cargador.prototype.initialize = function(){
-        console.log('cargador general');
-        
+        console.log("cargador general");
     };
-    Cargador.prototype.loadImagenes = function (lista){
-        this.cargadas   = 0;
-        this.totales    = lista.length;
-        for(i = 0; i < this.totales; i++){
+    
+    Cargador.prototype.loadImagenes = function(lista){
+        this.cargadas = 0;
+        this.totales = lista.length;
+        for(i= 0; i < this.totales; i++){
             this.cargaImagen(lista[i]);
         }
-        console.log('Inicia loadImagenes');
-    }
+        console.log("loadImagenes funciona" +" "+ lista);
+    };
     
-    Cargador.prototype.cargaImagen = function (){
+    Cargador.prototype.cargaImagen = function(ruta){
+        var image = new Image();
         var self = this;
-        var image = new image();
+        
         this[ruta] = image;
         image.onload = function(e){
             self.imagenCargada();
         }
         image.src = image.url = ruta;
-        console.log('inicia cargaImagenes');
-    }
+        console.log("cargaImagen funciona"+" "+ image.src);
+    };
     
     Cargador.prototype.imagenCargada = function(){
         this.cargadas ++;
         if(this.cargadas == this.totales){
             if(this.onComplete){
-                this.onComplete
+                this.onComplete();
             }else{
-                console.log('onComplete no definida');
+                console.log("onComplete no definida en App");
             }
         }
+        console.log("imagenCargada funciona");
+    };
     
-    }
-});
+    scope.Cargador = Cargador;
+}(window));
